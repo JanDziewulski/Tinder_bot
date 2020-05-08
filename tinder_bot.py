737452d3587d403bc36a28dab99ee849ae7af74f
jdziewulski <jandziewulski92@gmail.com>
2020-05-08 20:41:27 +0200

    login

--- tinder_bot.py
+++ tinder_bot.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+from time import sleep
+from config import password, login
 
 class TinderBot():
     def __init__(self):
@@ -10,36 +11,51 @@ class TinderBot():
 
     def login(self):
         self.driver.get('https://tinder.com')
-"""Logowanie się do aplikacji desktopowej"""
-        fb_btn = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/div[2]/button')
-        fb_btn.click()
+        sleep(2)
+# """Logowanie się do aplikacji desktopowej"""
+#         fb_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+#         fb_btn.click()
 
-"""Akceptowanie plików cookies"""
-        accept = bot.driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
-        accept.click()
+# """Akceptowanie plików cookies"""
+#         accept = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+#         accept.click()
+# """Wybieranie logowania za pomocą facebook'a"""
 
-"""Wybieranie logowania za pomocą facebook'a"""
 
-        fb_lc = bot.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[2]/button')
+        fb_lc = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/div[2]/button')
+
         fb_lc.click()
 
-"""Wybranie okna do logowania"""
+# """Wybranie okna do logowania"""
 
         base_window = self.driver.window_handles[0]
         popup = self.driver.switch_to_window(self.driver.window_handles[1])
-"""Wprowadzanie maila oraz hasla"""
+# """Wprowadzanie maila oraz hasla"""
 
         email_in = self.driver.find_element_by_xpath('//*[@id="email"]')
-        email_in.send_keys('jasto@vp.pl')
+        email_in.send_keys(login)
         pw_in = self.driver.find_element_by_xpath('//*[@id="pass"]')
-        pw_in.send_keys('dziewulszczak123')
+        pw_in.send_keys(password)
 
-"""Logowanie do platformy"""
+# """Logowanie do platformy"""
         login_bnt = self.driver.find_element_by_xpath('//*[@id="loginbutton"]')
-        log_in.click()
+        login_bnt.click()
+
+# """Zatwierdzenie lokalizacji """
+        loc_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+        loc_popup.click()
+
+        np_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+        np_popup.click()
+
+
+        self.driver.switch_to_window(base_window)
+        popup_1 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+        np_popup.click()
+
+        """ Przełączenie na kolejne okienko dialogowe oraz
+         potwierdzenie wtyczki umożliwiającej zaczytanie lokalizacji 
+        """
 
 
 
-
-bot = TinderBot()
-bot.login()
